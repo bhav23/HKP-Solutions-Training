0830e43d28bab09d6fef1f21c6ad6e83eb1d3a77
YiYang02 <69444243+YiYang02@users.noreply.github.com>
2020-12-17 23:05:02 -0500

    Update Java Refresher.java

--- Day 1/Java Refresher.java
+++ Day 1/Java Refresher.java
@@ -1,3 +1,38 @@
 /*
 What is Java?
+  -[Beginner-friendly] Easy to learn and understand
+  -[Secure] Designed to be platform-independent and secure, using virtual machine
+      -Why is Platform Independence important?
+          -Many programming languages needa compiler to reduce code into machine
+          language that user device can understand.
+          -Problem is many devices have diff machine languages to compile into 
+          -Solution is java compilers convert java code to bytecode which is
+          interpreted by a Java Virtual Machine [Dalvik VM]. As long as system has
+          Dalvik VM, Java code can run.
+          
+      -Why is Java Secure?
+        -Java apps run on virtual and is separate from the actual device
+          -Operating system can shut down Android apps if they become exceed 
+          certain limits
+  -Object oriented programming
+*/
+
+/*
+Why is Java easy to learn?
+
+ +Vast amt of resources to learn: websites, books, tutorials, classes
+ +Similar syntax to other popular programming languages
+ +One of the most human-readable languages
+ 
+char character = 'a';
+
+if (character == 'a')
+{
+  doSomething();
+}
+ else
+{
+  doSomethingElse();
+}
+
 */
